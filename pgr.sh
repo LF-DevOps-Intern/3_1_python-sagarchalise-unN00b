d5f3e3ea32c6cb9f510c18ff4120622797ed318d
unN00b <pranavpudasaini01@gmail.com>
2021-11-10 23:02:34 +0545

    Beautify wrapper help menu

--- pgr.sh
+++ pgr.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 show_help(){
-    echo -e "usage: $0 url [true|false]\n"
-    echo -e "description:\n"
-    echo -e "\turl \t\tURL to the remote file\n"
-    echo -e "\ttrue|false \tWhether to serve the file after download\n"
+    echo -ne "usage: $0 url [true|false]\n"
+    echo -ne "description:\n"
+    echo -ne "\turl \t\tURL to the remote file\n"
+    echo -ne "\ttrue|false \tWhether to serve the file after download\n"
 }
 
 if [[ $# -eq 0 || "$1" == "-h" ]]; then
