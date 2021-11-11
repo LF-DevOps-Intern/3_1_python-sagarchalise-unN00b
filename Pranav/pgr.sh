1123918f9547929c108f9c92dee96073fd9d1f4c
DroNepal <pranavpudasaini01@gmail.com>
2021-11-11 12:29:49 +0545

    Add support to virtualenv

--- Pranav/pgr.sh
+++ Pranav/pgr.sh
@@ -7,13 +7,38 @@ show_help(){
     echo -ne "\ttrue|false \tWhether to serve the file after download\n"
 }
 
+activate_venv(){
+    ENVDIR="$HOME/.venv/pygetremote"
+
+    # Create directory and initialize virtualenv if it doesn't exist
+    if [[ ! -d $ENVDIR ]]; then
+        mkdir -p $ENVDIR
+        virtualenv $ENVDIR -p 3.6
+        new=1
+    fi
+
+    source $ENVDIR/bin/activate
+
+    if [[ $new ]]; then
+        python -m pip install -r requirements.txt
+    fi
+}
+
+URL=$1
+SERVE_FLAG=$2
+
 if [[ $# -eq 0 || "$1" == "-h" ]]; then
     show_help
     exit 0
 fi
 
-URL=$1
-SERVE_FLAG=$2
+
+activate_venv &> /dev/null
+
+if [[ $? -ne 0 ]]; then
+    echo "Failed to initialize virtual environment at $ENVDIR"
+    exit 1
+fi
 
 if [[ "$SERVE_FLAG" == "true" ]]; then
     python3 pgr.py --url=$URL --http_server
